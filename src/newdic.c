436b844561033552a581e18045719415f8208d5b
Vlad Berejinsckij <johnytynky@gmail.com>
2017-04-25 20:20:49 +0700

    Code style

--- src/newdic.c
+++ src/newdic.c
@@ -4,24 +4,26 @@
 
 int main()
 {
-int number=0;
-char dirname[30]="./dictionary/";
-char r[5]=".txt";
-char namefile[10];
-char word[100];
-printf("Enter file name\n");
-scanf("%s", &namefile);
-strncat(namefile, r, 4);
-strncat(dirname, namefile, 10);
-FILE *pf;
-pf=fopen(dirname, "w+");
-printf("Enter the number of words you want to enter\n");
-scanf("%i", &number);
-printf("Enter words (english.russian)\n");
-for(int i=0;i<number;i++){
-scanf("%s", &word);
-fprintf(pf, "%s\n", word);}
-fclose(pf);
-printf("Dictionary %s created\n", namefile);
-return 0;
+    int number = 0;
+    char dirname[30] = "./dictionary/";
+    char r[5] = ".txt";
+    char namefile[10];
+    char word[100];
+    printf("Enter file name\n");
+    scanf("%s", &namefile);
+    strncat(namefile, r, 4);
+    strncat(dirname, namefile, 10);
+    FILE *pf;
+    pf = fopen(dirname, "w+");
+    printf("Enter the number of words you want to enter\n");
+    scanf("%i", &number);
+    printf("Enter words (english.russian)\n");
+    for (int i = 0; i < number; i++) {
+        scanf("%s", &word);
+        fprintf(pf, "%s\n", word);
+    }
+    fclose(pf);
+    printf("Dictionary %s created\n", namefile);
+    return 0;
 }
+
