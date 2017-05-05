993ea93e181a62b84f8f2957366d5a56a4269960
Vlad Berejinsckij <johnytynky@gmail.com>
2017-05-05 20:53:54 +0700

    Create the checkFile function

--- src/newdic.c
+++ src/newdic.c
@@ -30,6 +30,32 @@ void createFile(char* filepath, char* word, char* namefile, int &number){
     cout << "Dictionary " << namefile << " created\n";
 }
 
+int checkFile(char* filepath) {
+	char otvet;
+	int number=0;
+   ifstream file;
+   file.open(filepath);
+   if(file)
+      {cout << "File is exist. Do you want to replace this file? (Y/N)\n";
+      cin >> otvet;
+       if (otvet == 'Y' || otvet == 'y')
+            return 1;
+         else {if (otvet == 'N' || otvet == 'n') 
+            return 2;
+        else do{
+		if (otvet == 'Y' || otvet == 'y'){
+		number=1;
+            return 1;}
+        else {if (otvet == 'N' || otvet == 'n') {
+		number=1;
+            return 2;}
+        else cout << "Enter Y or N !!!\nType your answer: ";}
+        	cin >> otvet;
+		}while(number!=1);
+		}
+} else return 3;
+}
+
 int main()
 {
     int number = 0;
@@ -43,8 +69,38 @@ int main()
     strncat(filepath, dirname, 30);
     strncat(filepath, namefile, 10);
     strncat(filepath, r, 4); 
-checkNumber (number);
-createFile(filepath, word, namefile, number);
+switch (checkFile(filepath))
+{
+	case 1:
+		{
+		checkNumber(number);
+		createFile(filepath, word, namefile, number);	
+		break;
+		}
+	case 2:
+		{
+		cout << "Enter file name\n";
+		namefile[0]=0;
+		filepath[0]=0;
+		cin >> namefile;
+		strncat(filepath, dirname, 30);
+    	strncat(filepath, namefile, 10);
+    	strncat(filepath, r, 4);    
+    	checkNumber(number);
+    	createFile(filepath, word, namefile, number);
+    	break;
+		}
+	case 3:
+		{
+		checkNumber(number);
+		createFile(filepath, word, namefile, number);
+		break;
+		}
+	case 4:
+		{
+		cout << "Uncorrect\n";
+		break;
+		}
+}
     return 0;
 }
-
