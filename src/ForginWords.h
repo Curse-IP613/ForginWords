b12d556d793963e5f84d09eade930a7311fa3f06
Vlad Berejinsckij <johnytynky@gmail.com>
2017-05-08 20:27:15 +0700

    Add function NewDic to StartMenu

---
+++ src/ForginWords.h
@@ -0,0 +1,7 @@
+#ifndef newdic
+#define newdic
+void checkNumber (int &number);
+void createFile(char* filepath, char* word, char* namefile, int &number);
+int checkFile(char* filepath);
+int NewDic();
+#endif
--- src/StartMenu.c
+++ src/StartMenu.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <iostream>
+#include "ForginWords.h"
+#include "newdic.c"
+using namespace std;
 
-main()
+int main()
 {
     int Number;
     printf("1-Choice dictionary \n");
@@ -13,8 +17,11 @@ main()
         printf("Func 1 \n");
         break;
     case 2:
+{
         printf("Func 2 \n");
+	NewDic();
         break;
+}
     case 0:
         return 0;
         break;
