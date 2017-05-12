f8e38c9bf46591a1af627b792da570b4685d497e
Artyom <seminartem98@mail.ru>
2017-05-12 13:52:19 +0700

    Create StartMenu-test.c and Edit StartMenu.c

--- src/StartMenu.c
+++ src/StartMenu.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+
+int switchfunc(int x);
+int Number;
 main()
 {
-    int Number;
+	int x;
+    //int Number;
+   // scanf("%i", &x);
     printf("1-Choice dictionary \n");
     printf("2-Create personal dictionary \n");
     printf("0-Exit \n");
     printf("Enter number point menu: \n");
-    scanf("%i", &Number);
-    switch (Number) {
+    scanf("%i", &x);
+    switchfunc(x);
+    
+  /*  switch (y) 
+	{
     case 1:
         printf("Func 1 \n");
         break;
@@ -18,6 +26,28 @@ main()
     case 0:
         return 0;
         break;
-    }
+    }	
+}
+*/
+}
+int switchfunc(x){
+
+
+//scanf("%i", &Number);
+	Number = x;
+	switch (Number) 
+	{
+    case 1:
+       printf("Func 1 \n");
+        return 1;
+        break;
+    case 2:
+       printf("Func 2 \n");
+        return 2;
+        break;
+    case 0:
+        return 0;
+        break;
+    }	
 }
 
