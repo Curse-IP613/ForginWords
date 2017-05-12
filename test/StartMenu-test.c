2f038ba448da46c90bbbf80c7848834f6ff30a94
Artyom <seminartem98@mail.ru>
2017-05-12 13:54:06 +0700

    Create test for StartMenu

--- test/StartMenu-test.c
+++ test/StartMenu-test.c
@@ -1,1 +1,19 @@
+#include <ctest.h>
+#include "../src/StartMenu.c"
+
+CTEST(test_number_one, switchfunc)
+{
+    // Given
+    const int a = 1;
+    
+    
+    // When
+    int result = switchfunc (a);
+
+    // Then
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+///************************************///
 
