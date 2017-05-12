371bc84688e1bb584be63ac51ab7704f572fe297
Artyom <seminartem98@mail.ru>
2017-05-12 13:54:56 +0700

    Add some more test for StartMenu.c

--- test/StartMenu-test.c
+++ test/StartMenu-test.c
@@ -17,3 +17,33 @@ CTEST(test_number_one, switchfunc)
 
 ///************************************///
 
+
+CTEST(test_number_one, switchfunc)
+{
+    // Given
+    const int a = 2;
+    
+    
+    // When
+    int result = switchfunc (a);
+
+    // Then
+    int expected = 2;
+    ASSERT_EQUAL(expected, result);
+}
+
+///************************************///
+
+CTEST(test_number_one, switchfunc)
+{
+    // Given
+    const int a = 0;
+    
+    
+    // When
+    int result = switchfunc (a);
+
+    // Then
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
